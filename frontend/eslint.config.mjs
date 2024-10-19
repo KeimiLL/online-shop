bc4faae2cddffceb0a5518c23e7af0f458b42b7c
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2024-10-19 13:29:04 +0200

    OS-3: Add eslint and precommit

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,11 @@
+import ("globals");
+import ("@eslint/js");
+import ("typescript-eslint");
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
