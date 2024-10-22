2b2aa5daddaf8b468df262b4cb635f1101d840ae
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2024-10-22 19:58:05 +0200

    OS-5: Add basic login page
    - Add Angular Material
    - Update yarn packages
    - Add new material theme

--- frontend/src/app/core/auth/components/login/login.component.ts
+++ frontend/src/app/core/auth/components/login/login.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  protected myForm: FormGroup;
 
-
+  constructor() {
+    this.myForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
 }
