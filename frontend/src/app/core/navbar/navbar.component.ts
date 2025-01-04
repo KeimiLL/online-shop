75a2cf31fa62e8592dc74a0784078861a3c4ed8c
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2025-01-04 15:41:33 +0100

    OS-7: Add new navbar with transitions

--- frontend/src/app/core/navbar/navbar.component.ts
+++ frontend/src/app/core/navbar/navbar.component.ts
@@ -1,14 +1,49 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import {
+    ChangeDetectionStrategy,
+    Component,
+    HostListener,
+    inject,
+    OnInit,
+    signal,
+} from "@angular/core";
+import {
+    NavigationEnd,
+    Router,
+    RouterLink,
+    RouterModule,
+} from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: "app-navbar",
+    standalone: true,
+    imports: [RouterLink, RouterModule, CommonModule],
+    templateUrl: "./navbar.component.html",
+    styleUrl: "./navbar.component.scss",
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+    protected isAuth = signal<boolean>(false);
 
+    readonly #router = inject(Router);
+
+    ngOnInit(): void {
+        this.#router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe((event: NavigationEnd) => {
+                console.log(event.url);
+                this.isAuth.set(event.url === "/auth");
+            });
+    }
+
+    @HostListener("window:scroll", ["$event"])
+    protected onWindowScroll(): void {
+        const navbar = document.querySelector(".navbar") as HTMLElement;
+        if (window.scrollY > navbar.clientHeight || this.isAuth()) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
 }
